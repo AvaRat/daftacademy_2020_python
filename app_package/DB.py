046d25f85de675eeac0739c1e8778764681f6dfa
Marcel <marcelos.kalinski@gmail.com>
2020-05-02 21:23:24 +0200

    zad2 wyk_4

--- app_package/DB.py
+++ app_package/DB.py
@@ -1,10 +1,9 @@
 from typing import List, Optional
 import aiosqlite
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from fastapi.logger import logger
-
-from databases import Database
 
 
 # SQLAlchemy specific code, as with any other app
@@ -27,19 +26,29 @@ app = FastAPI()
 @app.on_event("startup")
 async def startup():
     app.db_connection = await aiosqlite.connect('chinook.db')
+    app.db_connection.row_factory = aiosqlite.Row
     logger.info("Connection to database established")
 
 
 
 @app.get("/tracks", response_model=List[Tracks])
 async def get_tracks(page: int = 0, per_page: int = 10):
-    app.db_connection.row_factory = aiosqlite.Row
-    cursor = await app.db_connection.execute("SELECT * FROM tracks ORDER BY TrackId LIMIT ? OFFSET ?;", (per_page, page))
+    cursor = await app.db_connection.execute("SELECT * FROM tracks ORDER BY TrackId LIMIT ? OFFSET ?;", (per_page, page-1))
     tracks = await cursor.fetchall()
     return tracks
+
+@app.get("/tracks/composers", response_model = List[str])
+async def get_tracks_composers(composer_name: str):
+    cursor = await app.db_connection.execute("SELECT name FROM tracks WHERE composer== ? ORDER BY name ASC", [composer_name])
+    tracks = await cursor.fetchall()
+    if(len(tracks) > 0):
+        return [track['name'] for track in tracks] 
+    response = JSONResponse(content={'detail':{'error':'Composer not found'}}, status_code=status.HTTP_404_NOT_FOUND)
+    return response
+    
+
 
 @app.on_event("shutdown")
 async def shutdown():
     await app.db_connection.close()
     logger.info("Connection to database closed")
-
