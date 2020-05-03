b29d62ff2dc119595d819c5cd3c34db110342f63
Marcel <marcelos.kalinski@gmail.com>
2020-05-03 16:21:00 +0200

    sortowanie po CustomerId

--- app_package/DB.py
+++ app_package/DB.py
@@ -162,7 +162,7 @@ async def get_sales_stats(category: str):
         return response
     cursor = await app.db_connection.execute("SELECT C.CustomerId, C.Email, C.Phone, SUM(I.Total) AS Sum \
                                                 FROM customers C INNER JOIN invoices I ON C.CustomerId=I.CustomerId \
-                                                GROUP BY C.CustomerId ORDER BY SUM(I.Total) DESC")
+                                                GROUP BY C.CustomerId ORDER BY SUM(I.Total) DESC, C.CustomerId ")
     sales_list = await cursor.fetchall()
     return sales_list
 
